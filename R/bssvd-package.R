736b8c1922303cb57339c85b90c15b79fe78f359
John Bryant <john.r.bryant@gmail.com>
2025-03-24 08:50:11 +0700

    modified cov yaml and wrote bssvd-package documentation

--- R/bssvd-package.R
+++ R/bssvd-package.R
@@ -9,8 +9,28 @@
 #' support package **bage**. They are designed for use
 #' by developers, not data analysts.
 #'
-#' @section Functions for 
-
+#' @section Human Fertility Database:
+#'
+#' - [data_ssvd_hfd()] Prepare HFD data to created scaled SVD object
+#' - [hfd_tidy()] Initial tidying of HFD data
+#' - [coef_hfd()] Extract coef from scaled SVD of HFD data
+#' - [asfr_subset()] Subset of HFD data
+#'
+#' @section Human Mortality Database:
+#'
+#' - [data_ssvd_hmd()] Prepare HMD data to created scaled SVD object
+#' - [coef_hmd()] Extract coef from scaled SVD of HMD data
+#'
+#' @section OECD Labour Force Participation data:
+#'
+#' - [data_ssvd_lfp()] Prepare labour force data to created scaled-SVD object
+#' - [coef_lfp()] Extract coef from scaled SVD of labour force data
+#' - [oecd_lfp_subset()] Subset of labour force data
+#'
+#' @section General purpose functions:
+#'
+#' - [make_matrix_and_offset()] Create components for scaled SVD object
+#'
 #' @keywords internal
 "_PACKAGE"
 
